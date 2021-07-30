39adae9b320b7886237b55753e746ae6e68444ef
Edilza Rodrigues Pannunzio <65309655+epannunzio@users.noreply.github.com>
2021-07-30 16:47:13 -0300

    Add files via upload

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { ButtonHTMLAttributes } from 'react'
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    isOutLined?: boolean
+};
+
+export function Button({ isOutLined = false, ...props }: ButtonProps) {
+    return (
+        <button 
+        className={`button ${isOutLined ? 'outLined' : ''}`} 
+        {...props} 
+        />
+    )
+}
+
---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,9 @@
+import { useContext } from 'react'
+import { AuthContext } from '../contexts/AuthContexts'
+
+export function useAuth() {
+  const value = useContext(AuthContext)
+
+  return value;
+}
+
